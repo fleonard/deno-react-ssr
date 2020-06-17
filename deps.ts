10fb554530fade8f19fbfdcd57f69e74ae10afc0
Francesco Leonardi <francesco@potatolondon.com>
2020-06-17 12:21:08 +0100

    fixed typo

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@
 import React from 'https://dev.jspm.io/react@16.13.1';
 // @deno-types="https://deno.land/x/types/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from 'https://dev.jspm.io/react-dom@16.13.1/server';
-export {React, ReactDOMServer }
+export { React, ReactDOMServer }
 export { Application, Context, Router } from 'https://deno.land/x/oak@v4.0.0/mod.ts';
